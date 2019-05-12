e948f009df046ed00a3119585dff165b06630b19
Mr <490724203@qq.com>
2019-05-12 22:09:59 +0800

    忽略某些用不到的模块的引入

--- demo4/webpack.dev.js
+++ demo4/webpack.dev.js
@@ -1,9 +1,28 @@
-let {smart} = require("webpack-merge") // 合并 根据不同环境找不同的配置 
+let { smart } = require("webpack-merge") // 合并 根据不同环境找不同的配置 
 // 命令行输入 npm run build -- --config webpack.dev.js
 let base = require("./webpack.base")
-module.exports = smart(base,{
-    mode:"development",
-    module:{
+let webpack = require("webpack")
+module.exports = smart(base, {
+    mode: "development",
+    module: {
         noParse: /jquery/  // 当这个包很大并且知道没有依赖第三方模块时， 可以使用noparse不去解析它的依赖， 让它提高打包速度
-    }
+    },
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_module/, // 排除这个文件， 不使用loader解析
+            include: path.resolve("src"), // 表示只有这些文件会用到这个loader解析
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: [
+                        '@babel/preset-env'
+                    ]
+                }
+            }
+        }
+    ],
+    plugins: [
+        new webpack.IgnorePlugin(/\.\/local/, /moment/) // moment内部会引用 local 模块, 各种语言都含有， 这样就可以使其忽略该模块，然后自己手动引入需要的模块
+    ]
 })
